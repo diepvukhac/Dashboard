9999d06453a4058012e7c3f339760cf52494d58a
Salmon <154438238+diepvukhac@users.noreply.github.com>
2024-01-30 17:08:47 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,41 @@
+// AdminHub icon rotation
+function addSpinClass(element) {
+    element.classList.add('bx-spin');
+  }
+  
+  function removeSpinClass(element) {
+    element.classList.remove('bx-spin');
+  }
+// AdminHub icon rotation
+ 
+
+// #sider-bar .side-menu li.active{
+//     background: var(--grey);
+//     position: relative;
+
+// }
+
+// #sider-bar .side-menu li.active::before{
+//     content: '';
+//     position: absolute;
+//     width: 40px;
+//     height: 40px;
+//     border-radius: 50%;
+//     top: -40px;
+//     right: 0;
+//     box-shadow: 20px 20px 0 var(--grey);
+//     z-index: -1;
+
+// }
+
+// #sider-bar .side-menu li.active::after{
+//     content: '';
+//     position: absolute;
+//     width: 40px;
+//     height: 40px;
+//     border-radius: 50%;
+//     top: -40px;
+//     right: 0;
+//     box-shadow: 20px 20px 0 var(--grey);
+//     z-index: -1;
+// }
